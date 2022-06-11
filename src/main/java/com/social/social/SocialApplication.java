567050a2563e90428767500a1f5e4a35999510f9
Miguel Rguez <uo263494@uniovi.es>
2022-06-11 17:21:03 +0200

    feat: spring boot security

--- src/main/java/com/social/social/SocialApplication.java
+++ src/main/java/com/social/social/SocialApplication.java
@@ -2,12 +2,7 @@ package com.social.social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.bson.Document;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 @SpringBootApplication
@@ -16,11 +11,6 @@ public class SocialApplication {
     public static void main(String[] args) {
         SpringApplication.run(SocialApplication.class, args);
 
-//        String connectionString = System.getenv("mongoUriSocial");
-//        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-//            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<Document>());
-//            databases.forEach(db -> System.out.println(db.toJson()));
-//        }
     }
 
 }
